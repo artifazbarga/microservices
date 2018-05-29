addff82a54c2e2f3b1f31b82352839a336449987
TomerAdmon <tomer.a@quali.com>
2018-05-29 19:38:10 +0300

    add feature to movies service

--- services/movies.py
+++ services/movies.py
@@ -1,41 +1,62 @@
+import uuid
+
 from services import root_dir, nice_json
-from flask import Flask
+from flask import Flask, request
 from werkzeug.exceptions import NotFound
 import json
 
-
 app = Flask(__name__)
 
-with open("{}/database/movies.json".format(root_dir()), "r") as f:
-    movies = json.load(f)
+ROOT_MOVIES = {"uri": "/", "subresource_uris": {"movies": "/movies", "movie": "/movies/<id>"}}
+MOVIES_JSON = "{}/database/movies.json".format(root_dir())
+
+
+def get_movies(movie_id=None):
+    with open(MOVIES_JSON, "r") as f:
+        movies = json.load(f)
+        if movie_id is None:
+            return movies
+        else:
+            return movies[movie_id]
+
+
+get_movies()
 
 
 @app.route("/", methods=['GET'])
-def hello():
-    return nice_json({
-        "uri": "/",
-        "subresource_uris": {
-            "movies": "/movies",
-            "movie": "/movies/<id>"
-        }
-    })
+def root():
+    return nice_json(ROOT_MOVIES)
+
 
 @app.route("/movies/<movieid>", methods=['GET'])
 def movie_info(movieid):
-    if movieid not in movies:
+    if movieid not in get_movies():
         raise NotFound
 
-    result = movies[movieid]
+    result = get_movies(movieid)
     result["uri"] = "/movies/{}".format(movieid)
 
     return nice_json(result)
 
 
-@app.route("/movies", methods=['GET'])
+@app.route("/movies", methods=['GET', 'POST'])
 def movie_record():
-    return nice_json(movies)
+    if request.method == 'POST':
+        id = str(uuid.uuid4())
+        movie = {'title': request.form['title'],
+                 'id': id,
+                 'rating': request.form['rating'],
+                 'director': request.form['director']}
+
+        data = get_movies()
+
+        data.update({id: movie})
+
+        with open(MOVIES_JSON, 'w') as f:
+            json.dump(data, f, sort_keys=True, indent=4)
+
+    return nice_json(get_movies())
 
 
 if __name__ == "__main__":
-    app.run(port=5001, debug=True)
-
+    app.run(host='0.0.0.0', port=5001, debug=True)
